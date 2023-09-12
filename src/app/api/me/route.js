ff726c368edc198b7a497e75fa10c75e79ad1df7
noelbluu <panida.stp@icloud.com>
2023-09-12 23:24:22 +0700

    update GET me

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   return NextResponse.json({
     ok: true,
-    fullName: "Chayanin Suatap",
-    studentId: "650610560",
+    fullName: "Panda Suthapakti",
+    studentId: "650610790",
   });
 };
